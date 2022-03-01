fa81cd366c758ebcbddc09d88ef608c7e385fbf4
Star1515 <85513028+Star1515@users.noreply.github.com>
2022-03-01 15:34:03 +0300

    Домашнее задание 2

---
+++ js/main.js
@@ -0,0 +1,22 @@
+import { printError, printREsult } from './printResult.js';
+import getDateDiff from './getDateDiff.js';
+import {input, blockTime, interval, a, b, c} from './timer.js';
+
+const form = document.getElementById('datecalc');
+
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+
+  const firsDate = formData.get('firstDate');
+  const secondDate = formData.get('secondDate');
+
+  if (!firsDate || !secondDate) {
+    printError('Oooooopppsss - введите текст!!!!');
+  } else {
+    const dateDiff = getDateDiff(firsDate, secondDate);
+    printREsult(dateDiff);
+  }
+};
